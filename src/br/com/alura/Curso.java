2089f42bbd2617574881bf9856dfe312cb26647b
Marcelo Alves Nogueira <mclg.nogueira@gmail.com>
2023-03-26 14:28:35 -0300

    Trabalhando com Mapas.

--- src/br/com/alura/Curso.java
+++ src/br/com/alura/Curso.java
@@ -2,10 +2,12 @@ package br.com.alura;
 
 import java.util.Collections;
 import java.util.HashSet;
+import java.util.LinkedHashMap;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
+import java.util.NoSuchElementException;
 import java.util.Set;
-import java.util.TreeSet;
 
 public class Curso {
 	
@@ -13,7 +15,8 @@ public class Curso {
 	private String instrutor;
 	private List<Aula> aulas = new LinkedList<Aula>();
 	private Set<Aluno> alunos = new HashSet<>();
-
+	private Map<Integer, Aluno> matriculaParaAluno = new LinkedHashMap<>();
+	
 	public Curso(String nome, String instrutor) {
 		super();
 		this.nome = nome;
@@ -46,6 +49,7 @@ public class Curso {
 	
 	public void matricula(Aluno aluno) {
 		this.alunos.add(aluno);
+		this.matriculaParaAluno.put(aluno.getNumeroMatricula(), aluno);
 	}
 	
 	public Set<Aluno> getAlunos() {
@@ -55,5 +59,16 @@ public class Curso {
 	public boolean estaMatriculado(Aluno aluno) {
 		return this.alunos.contains(aluno);
 	}
-
+	public Aluno buscaMatriculado(int numero) {
+//		for (Aluno aluno : alunos) {
+//			if(aluno.getNumeroMatricula() == numero);
+//			return aluno;
+//		}
+//		throw new NoSuchElementException("Matricula não encontrada!"+ numero);
+		
+		if(!matriculaParaAluno.containsKey(numero))
+			throw new NoSuchElementException();
+		return matriculaParaAluno.get(numero);
+	}
+	
 }
--- src/br/com/alura/QualColecaoUsar.java
+++ src/br/com/alura/QualColecaoUsar.java
@@ -1,10 +1,15 @@
 package br.com.alura;
 
+import java.util.Collection;
+import java.util.HashSet;
+
 public class QualColecaoUsar {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Collection<Aluno> alunos = new HashSet<>();
+		alunos.size();
+		//alunos.get(56);
 	}
 
 }
